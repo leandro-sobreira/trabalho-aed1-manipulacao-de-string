b0c027505aa8ac1cccbd14ca944cf8cf430b2225
leandro-sobreira <lperes16623@gmail.com>
2024-06-17 09:37:42 -0400

    Buscar quase pronto

--- main.c
+++ main.c
@@ -12,31 +12,67 @@ int main(){
     int vetFlag = 0;
 
     do{ // Loop principal
-        do{ // Loop de inserção
-            printf( "1 - Inserir\n"
-                    "2 - Buscar\n"
-                    "3 - Listar Ordenado\n"
-                    "4 - Sair\n"
-            );
-            scanf("%d", &opc);
-        }while (opc < 1 && opc > 4);
-
+        printf( "1 - Inserir\n"
+                "2 - Buscar\n"
+                "3 - Listar Ordenado\n"
+                "4 - Sair\n"
+                "O que quer fazer? "
+        );
+        scanf("%d", &opc);
         switch (opc)
         {
-            case 1:
+            case 1: // Inserir
                 for (int i = 0; i < TAM; i++)
                 {
                     char continuar;
                     printf("Insira o valor da posicao vet[%d]: ", i);
                     scanf("%d", &vet[i]);
-                    printf("Quer continuar? (s/n)");
+                    printf("Quer continuar? (s/n): ");
                     scanf(" %c", &continuar);
                     if (continuar == 'n') break;
                 }
+                vetFlag = 1;
+            break;
+
+            case 2: // Busca
+                if (vetFlag == 0)
+                {
+                    printf("Primeiramente insira algo na string! \n");
+                }else{
+                    int posicao = -1;
+                    int busca;
+
+                    printf("Qual numero esta procurando? ");
+                    scanf("%d", &busca);
+                    for (int i = 0; i < TAM; i++)
+                    {
+                        posicao = -1;
+                        if (vet[i] == busca)
+                        {
+                            posicao = i;
+                        } else {
+                            posicao = -1;
+                        }
+                        if (posicao != -1)
+                        {
+                            printf("Valor encontrado em vet[%d]\n", i);
+                        }
+                    }
+                    if (posicao == -1)
+                    {
+                        printf("Valor nao encontrado\n");
+                    }
+                    
+                }
+            break;
+
+            case 4:
+                printf("Obrigado por usar nosso programa :D");
             break;
             
             default:
-                break;
+                printf("Opcao invalida");
+                system("pause");
         }
     }while (opc != 4);
     
